8d058d5203ba1db120f9f5750b375501b2e25e06
ejdutoit <35363300+ejdutoit@users.noreply.github.com>
2018-01-22 13:53:01 +0000

    Update stacked.py

--- stacked.py
+++ stacked.py
@@ -5,6 +5,6 @@ B = [5., 25., 50., 20.]
 
 X = range(4)
 
-plt.bar(X, A, color = 'b')
-plt.bar(X, B, color = 'r', bottom = A)
+plt.bar(X, A, color = (7,105,79)/255) #provide color as a tuple with RGB values - this is green
+plt.bar(X, B, color = (221,147,58)/255, bottom = A) # orange color
 plt.show()
